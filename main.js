740cceddafa5015b69b3addfc78f41b418874d2e
callum1h3 <33325442+callum1h3@users.noreply.github.com>
2023-04-04 16:12:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require("fs");
 const app =  express();
 var path = require('path');
+const bodyParser = require("body-parser")
 
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
@@ -11,8 +12,6 @@ app.post('/', function(req, res)
 {
     res.send('Data Received: ' + req.body.filter);
 })
-
-app.use(express.json());
 
 app.listen(3000, function()
 {
